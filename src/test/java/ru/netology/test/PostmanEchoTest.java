56cca904829f12476c59aed619aa444b1fba25a4
SergGrebnev <serggrebnev79@gmail.com>
2024-08-06 15:49:31 +0700

    Fixed TestPOST

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data1"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
